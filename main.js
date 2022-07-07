2f1bfd7608d4964b4e901b664093946e180ff8f9
alex <acepegra@gmail.com>
2022-07-07 15:50:24 +0200

    Version avec le clic api fonctionnel

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const urlAPI = "http://localhost:8888/api_php-supra/"
 const catListe = document.querySelector('.categories-list')
-console.log(catListe)
+const catTitle = document.querySelector('.categories-title')
+const prdListe = document.querySelector('.produits-list')
+console.log(prdListe)
 
 // Listing des catégories
 
@@ -10,13 +12,40 @@ fetch(urlAPI + 'categories')
         console.log(response);
         let template = ''
         response.data.forEach(category => {
-            template += `<li data-id="${category.id}">${category.label}</li>`
-    });
-    catListe.innerHTML = template
-})
+            template += `<li class="cat-items" data-id="${category.id}">${category.label}</li>`
+        });
+        catListe.innerHTML = template
+    })
 
     .catch(error => console.error(error))
 
-  
-    
+// récup d'une catégorie
+const fetchCategory = (id = 1) => {
+// Listing des catégories + PRODUIT
+fetch(urlAPI + 'categories/' + id)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        let template = ''
+        if(response.produits.data) {
+        
+        response.produits.data.forEach(produits => {
+            template += `<li data-id="${produits.id}">${produits.label}</li>`
+        });
+    }
+        prdListe.innerHTML = template
+        
+        catTitle.innerText = `"${response.data[0].label}"`
+    })
 
+    .catch(error => console.error(error))
+} 
+
+fetchCategory()
+
+catListe.addEventListener('click', (e) => {
+    if (e.target.classList.contains('cat-items')) {
+        //alert(e.target.dataset.id)
+        fetchCategory(e.target.dataset.id)
+    }
+})
